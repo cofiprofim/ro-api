6d1fa1ca23301d1a7b1201bb4fae6756763cafa5
cofiprofim <cofiprofim@gmail.com>
2025-06-07 23:41:49 +0200

    Added __getattribute__ method

--- roblox/robloxclient.py
+++ roblox/robloxclient.py
@@ -1,7 +1,7 @@
 from httpx import URL, Response, Client
 
 from .utils.subdomains import LoadUrl
-from .api.games import get_user_games
+from .api import GamesApi
 
 from typing import Union
 
@@ -12,9 +12,18 @@ class _CustomRobloxClient(Client):
         return super().request(method, str(url), **kwargs)
 
 
-class RobloxClient:
+class RobloxClient(GamesApi):
     def __init__(self, auth=None):
         self._client = _CustomRobloxClient(auth=auth)
 
     def request(self, method, url):
         return self._client.request(method, url)
+
+    def __getattribute__(self, item: str):
+        attr = super().__getattribute__(item)
+        from functools import partial
+        if hasattr(attr, "_IS_API"):
+            return partial(attr, client=self)
+
+        return attr
+
