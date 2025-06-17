e5c5ac66e16bbe9298d48a4a50f64ddd0e674d08
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-06-17 15:36:01 +0200

    Now inheriting _CustomRobloxClient class

--- roblox/robloxclient.py
+++ roblox/robloxclient.py
@@ -1,29 +1,48 @@
 from httpx import URL, Response, Client
+import functools
 
 from .utils.subdomains import LoadUrl
-from .api import GamesApi
+
 
 from typing import Union
 
+# def func(self):
+#     import importlib
+#
+#     mod = importlib.import_module("roblox.api.games")
+#
+#
+#     class t(type):
+#
+#
+#     class games(metaclass=t):
+#         pass
+#
+#     for n, f in mod.__dict__.items():
+#         if hasattr(f, "_IS_API"):
+#             setattr(games, n, f)
+#     return games
 
-class _CustomRobloxClient(Client):
+
+class RobloxClient(Client):
+    """
+
+    """
+
+    def __init__(self, auth=None):
+        super().__init__(auth=auth)
+
+    def __getattribute__(self, item: str):
+        attr = super().__getattribute__(item)
+
+        if hasattr(attr, "_IS_API"):
+            return functools.partial(attr, client=self)
+
+        return attr
+
     def request(self, method: str,
                 url: Union[URL, str, LoadUrl], **kwargs) -> Response:
         return super().request(method, str(url), **kwargs)
 
-
-class RobloxClient(GamesApi):
-    def __init__(self, auth=None):
-        self._client = _CustomRobloxClient(auth=auth)
-
-    def request(self, method, url):
-        return self._client.request(method, url)
-
-    def __getattribute__(self, item: str):
-        attr = super().__getattribute__(item)
-        from functools import partial
-        if hasattr(attr, "_IS_API"):
-            return partial(attr, client=self)
-
-        return attr
+    from .api.games import fetch_games, fetch_user_games
 
