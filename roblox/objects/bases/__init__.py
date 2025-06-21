7f88280ab95462c1b4bec8c73e5571b48a60a258
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-06-22 00:08:18 +0200

    Added ClientAttr and BaseItem class

--- roblox/objects/bases/__init__.py
+++ roblox/objects/bases/__init__.py
@@ -2,7 +2,31 @@
 
 """
 
-from .basegame import *
-from .baseuser import *
-from .baseplace import *
-from .basecreator import *
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from ... import RobloxClient
+from .. import param
+
+
+class ClientAttr:
+    """
+    Literal client attr for IDLE
+    """
+    _client: RobloxClient
+
+
+class BaseItem:
+    id: int
+
+    def __init_subclass__(cls, id_key: Optional[str] = "id", **kwargs):
+        cls._FIELDS = {"id": param(key_name=id_key)}
+        super().__init_subclass__(**kwargs)
+
+
+# from .basegame import *
+# from .baseuser import *
+# from .baseplace import *
+# from .basecreator import *
