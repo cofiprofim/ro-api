5a0877f171eb22c2a6bde6256401e6a46669e935
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-06-22 00:13:30 +0200

    More testing

--- examples/get_user_games.py
+++ examples/get_user_games.py
@@ -1,8 +1,15 @@
-from roblox import RobloxClient
+from roblox import *
+from roblox.objects.bases.basegame import BaseGame
 
 client = RobloxClient()
-games_iterator = client.fetch_games(324324)
+games_iterator = client.fetch_user_games(2525288738)
+
+for games_batch in games_iterator:
+    for game in games_batch:
+        game: BaseGame
+
+        if game.id == 2535663803:
+            print(game.fetch_game_media())
 
 
-print(games_iterator)
 
