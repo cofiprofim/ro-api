5105c4402dfb581eb23f827e1c9513c971f6518b
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-06-29 00:11:25 +0200

    Removed __getattribute__ dunder method

--- roblox/robloxclient.py
+++ roblox/robloxclient.py
@@ -1,30 +1,16 @@
 from httpx import URL, Response, Client
-import functools
 
-from .utils.subdomains import LoadUrl
-from .api.games import fetch_user_games
+from ._utilities.subdomains import LoadUrl
 
 from typing import Union
 
 
 class RobloxClient(Client):
-    """
-
-    """
-
     def __init__(self, auth=None):
         super().__init__(auth=auth)
-
-    def __getattribute__(self, item: str):
-        attr = super().__getattribute__(item)
-
-        if hasattr(attr, "_IS_API"):
-            return functools.partial(attr, client=self)
-
-        return attr
 
     def request(self, method: str,
                 url: Union[URL, str, LoadUrl], **kwargs) -> Response:
         return super().request(method, str(url), **kwargs)
 
-    fetch_user_games = fetch_user_games
+    from roblox.api.games import fetch_game_media, fetch_user_games, fetch_games
