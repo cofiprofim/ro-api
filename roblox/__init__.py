5ed7db23a95a0e3bb2aaf6d88672e6ef4544f939
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-06-28 23:49:06 +0200

    Version and name changes

--- roblox/__init__.py
+++ roblox/__init__.py
@@ -2,10 +2,10 @@
 
 """
 
-__title__ = "roblox"
+__title__ = "ro-api"
 __author__ = "cofiprofim"
 __license__ = ""
 __copyright__ = ""
-__version__ = "1.0.0"
+__version__ = "0.0.5"
 
 from .robloxclient import *
