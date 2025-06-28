1149347d534d9348893460b768d8f74f623091c2
cofiprofim <121694687+cofiprofim@users.noreply.github.com>
2025-06-28 23:48:37 +0200

    Files deleting to merge

--- roblox/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# from typing import Callable
-
-# def client_maker(func: Callable) -> Callable:
-#     def wrapper():
-#         func()
--- roblox/utils/subdomains/__init__.py
+++
@@ -1,4 +0,0 @@
-from .domain_maker import *
-
-thumbnails = LoadDomain("thumbnails", versions_limit=2)
-games = LoadDomain("games", versions_limit=2)
